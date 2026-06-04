e01b564c688d5ffdf714b8d63ee8dc970e1c9b51
ernie8bigbird <ernie8bigbird@hotmail.com>
2026-06-04 14:38:59 +1200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ernest Morgan on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
