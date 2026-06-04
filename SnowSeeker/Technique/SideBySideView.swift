b094309523472d808878cc7a8f1d5d3f45eb4ff6
ernie8bigbird <ernie8bigbird@hotmail.com>
2026-06-04 17:41:18 +1200

    playing with split navigation view (iPad)

---
+++ SnowSeeker/Technique/SideBySideView.swift
@@ -0,0 +1,18 @@
+//
+//  SideBySideView.swift
+//  SnowSeeker
+//
+//  Created by Ernest Morgan on 04/06/2026.
+//
+
+import SwiftUI
+
+struct SideBySideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideBySideView()
+}
