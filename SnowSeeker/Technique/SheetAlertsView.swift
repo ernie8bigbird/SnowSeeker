4d6a41d6a85939a825325448b65fdfae66edc3b5
ernie8bigbird <ernie8bigbird@hotmail.com>
2026-06-04 20:43:24 +1200

    sheets and alerts view

--- SnowSeeker/Technique/SheetAlertsView.swift
+++ SnowSeeker/Technique/SheetAlertsView.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct SheetAlertsView: View {
+    
+    @State private var selectedUser: User? = nil
+    @State private var isShowingUser = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap Me") {
+            selectedUser = User()
+            isShowingUser = true
+        }
+        .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
+            Button(user.id) { }
+        }
     }
 }
 
--- SnowSeeker/Technique/SideBySideView.swift
+++ SnowSeeker/Technique/SideBySideView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SideBySideView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView(columnVisibility: .constant(.all)) {
+            NavigationLink("Primary") {
+                Text("New View")
+            }
+        } detail: {
+            Text("Some Content")
+        }
+        .navigationSplitViewStyle(.balanced)
     }
 }
 
