d2ce8c50e6a661b37833121c4d0838ec5e279046
ernie8bigbird <ernie8bigbird@hotmail.com>
2026-06-04 20:42:25 +1200

    alert was a sheet

---
+++ SnowSeeker/Technique/SheetAlertsView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetAlertsView.swift
+//  SnowSeeker
+//
+//  Created by Ernest Morgan on 04/06/2026.
+//
+
+import SwiftUI
+
+struct SheetAlertsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetAlertsView()
+}
