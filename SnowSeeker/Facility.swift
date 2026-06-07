a38075eee35a9bc8fc3e2356438b8cfc1f2520e4
ernie8bigbird <ernie8bigbird@hotmail.com>
2026-06-08 07:23:53 +1200

    adding icons for facilities

---
+++ SnowSeeker/Facility.swift
@@ -0,0 +1,8 @@
+//
+//  Facility.swift
+//  SnowSeeker
+//
+//  Created by Ernest Morgan on 08/06/2026.
+//
+
+import Foundation
---
+++ SnowSeeker/ResortView.swift
@@ -0,0 +1,63 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Ernest Morgan on 06/06/2026.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    
+    let resort: Resort
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                        VStack(spacing:10) {
+                            ResortDetailsView(resort: resort)
+                        }
+                        VStack(spacing:10) {
+                            SkiDetailsView(resort: resort)
+                        }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
+                    
+                    
+                }
+                .padding(.vertical)
+                .background(.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    Text("Facilities")
+                        .font(.headline)
+                    Text(resort.facilities, format: .list(type: .and))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+                
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    ResortView(resort: .example)
+}
